4984ada41af5d6aaceae5ff29775f6de1a235791
omaryahir <1206997+omaryahir@users.noreply.github.com>
2019-09-28 17:53:36 -0500

    ADD: Creating the initial UI

---
+++ intro-mern/intro-mern-frontend/src/Products/components/ProuctLayout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import AddButton from './AddButton'
+import Loading from './Loading'
+
+const ProductLayout = () => {
+    return <>
+        <Header title="Products app" />
+        <AddButton />
+        <Loading />
+    </>
+}
+
+export default ProductLayout
